5df43ed66dbc59e4ed7bd19f3f5da0aa96ec8e84
Baxromjon <73574634+Baxromjon@users.noreply.github.com>
2023-04-28 15:43:35 +0500

    dynamic link

--- lib/my_book.dart
+++ lib/my_book.dart
@@ -5,150 +5,11 @@ import 'package:flutter/material.dart';
 
 import 'firebase_repo.dart';
 
-class MyBooks extends StatelessWidget {
+class MyBooks extends StatefulWidget {
   const MyBooks({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final moviesRef = FirebaseFirestore.instance
-        .collection('bookId')
-        .withConverter<Book>(
-      fromFirestore: (snapshots, index) => Book.fromJson(snapshots.data()!),
-      toFirestore: (movie, index) => movie.toJson(),
-    );
-    return Scaffold(
-      // appBar: AppBar(
-      //   title: Text(
-      //     "Books",
-      //     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-      //   ),
-      //   actions: [
-      //     GestureDetector(
-      //       onTap: () {
-      //         Navigator.push(context,
-      //             MaterialPageRoute(builder: (context) => InputPage()));
-      //       },
-      //       child: Icon(
-      //         Icons.search,
-      //         size: 32,
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       width: 20,
-      //     ),
-      //   ],
-      // ),
-      body: StreamBuilder<QuerySnapshot<Book>>(
-          stream: moviesRef.snapshots(),
-          builder: (context, snapshot) {
-            final data = snapshot.requireData;
-            return ListView.separated(
-                separatorBuilder: (context, index) {
-                  return Divider(
-                    height: 12,
-                  );
-                },
-                itemCount: data.size,
-                itemBuilder: (BuildContext context, int index) {
-                  return buildUIMovie(snapshot.data!.docs[index].data(),context);
-                });
-          }),
-    );
-  }
-  @override
-  Widget buildUIMovie(Book movie,BuildContext context) {
-    return InkWell(
-      onTap: (){
-        // Navigator.push(context, MaterialPageRoute(builder: (context)=>ViewShow(name: movie.number.toString(), price: movie.price.toString(),)));
-      },
-      child: Container(
-        height: 150,
-        child: Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: Card(
-            clipBehavior: Clip.none,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
-            elevation: 12,
-            child: Row(
-              children: [
-                // Container(
-                //     height: 120,
-                //     child: Image.network( movie.image.toString())),
-
-                SizedBox(
-                  width: 16,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      movie.name.toString(),
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    InkWell(
-                      onTap: (){
-                        getDynamicUrl("http://exampleoson.page.link"+"${movie.name.toString()}");
-                      },
-
-                      child: Text(
-                        movie.number.toString(),
-                        style: TextStyle(fontSize: 22, color: Colors.lightBlueAccent),
-                      ),
-                    ),
-                    InkWell(
-                      onTap: (){
-                        buildDynamicLinks(movie.name.toString(), movie.number.toString(), movie.id.toString());
-                      },
-                      child: Text(
-                        movie.id.toString(),
-                        style: TextStyle(fontSize: 22, color: Colors.lightBlueAccent),
-                      ),
-                    ),
-
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-  buildDynamicLinks(String title,String image,String docId) async {
-    FirebaseDynamicLinks dynamicLinks = FirebaseDynamicLinks.instance;
-
-    String url = "http://exampleoson.page.link";
-    final DynamicLinkParameters parameters = DynamicLinkParameters(
-      uriPrefix: url,
-      link: Uri.parse('$url/$docId'),
-      androidParameters: AndroidParameters(
-        packageName: "com.example.example",
-        minimumVersion: 0,
-      ),
-      // iosParameters: IosParameters(
-      //   bundleId: "Bundle-ID",
-      //   minimumVersion: '0',
-      // ),
-      socialMetaTagParameters: SocialMetaTagParameters(
-          description: '',
-          imageUrl:
-          Uri.parse("$image"),
-          title: title),
-    );
-    ShortDynamicLink shortDynamicLink = await dynamicLinks.buildShortLink(parameters);
-
-
-    String? desc = shortDynamicLink.shortUrl.toString();
-    print(desc);
-
-    // await Share.share(desc, subject: title,);
-
-  }
-
-
+  State<MyBooks> createState() => _MyBooksState();
 
   static Future<String?> getDynamicUrl(String link) async {
     FirebaseDynamicLinks dynamicLinks = FirebaseDynamicLinks.instance;
@@ -188,4 +49,135 @@ class MyBooks extends StatelessWidget {
   }
 
 
+}
+
+class _MyBooksState extends State<MyBooks> {
+
+  @override
+  Widget build(BuildContext context) {
+    final moviesRef = FirebaseFirestore.instance
+        .collection('bookId')
+        .withConverter<Book>(
+      fromFirestore: (snapshots, index) => Book.fromJson(snapshots.data()!),
+      toFirestore: (movie, index) => movie.toJson(),
+    );
+
+    return Scaffold(
+
+      body:
+
+
+      StreamBuilder<QuerySnapshot<Book>>(
+          stream: moviesRef.snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Text('Loading...');
+            }
+            if (snapshot.hasError) {
+              return const Text('Something went wrong.');
+            }
+            final data = snapshot.requireData;
+            return ListView.separated(
+                separatorBuilder: (context, index) {
+                  return Divider(
+                    height: 12,
+                  );
+                },
+                itemCount: data.size,
+                itemBuilder: (BuildContext context, int index) {
+                  return buildUIMovie(snapshot.data!.docs[index].data(),context);
+                });
+          }),
+    );
+  }
+
+  @override
+  Widget buildUIMovie(Book movie,BuildContext context) {
+    return InkWell(
+      onTap: (){
+        // Navigator.push(context, MaterialPageRoute(builder: (context)=>ViewShow(name: movie.number.toString(), price: movie.price.toString(),)));
+      },
+      child: Container(
+        height: 150,
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Card(
+            clipBehavior: Clip.none,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+            elevation: 12,
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 16,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      movie.name.toString(),
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: (){
+                        MyBooks.getDynamicUrl("http://exampleoson.page.link"+"${movie.name.toString()}");
+                      },
+
+                      child: Text(
+                        movie.number.toString(),
+                        style: TextStyle(fontSize: 22, color: Colors.lightBlueAccent),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: (){
+                        buildDynamicLinks(movie.name.toString(), movie.number.toString(), movie.id.toString());
+                      },
+                      child: Text(
+                        movie.id.toString(),
+                        style: TextStyle(fontSize: 22, color: Colors.lightBlueAccent),
+                      ),
+                    ),
+
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  buildDynamicLinks(String title,String image,String docId) async {
+    FirebaseDynamicLinks dynamicLinks = FirebaseDynamicLinks.instance;
+
+    String url = "http://exampleoson.page.link";
+    final DynamicLinkParameters parameters = DynamicLinkParameters(
+      uriPrefix: url,
+      link: Uri.parse('$url/$docId'),
+      androidParameters: AndroidParameters(
+        packageName: "com.example.example",
+        minimumVersion: 0,
+      ),
+      // iosParameters: IosParameters(
+      //   bundleId: "Bundle-ID",
+      //   minimumVersion: '0',
+      // ),
+      socialMetaTagParameters: SocialMetaTagParameters(
+          description: '',
+          imageUrl:
+          Uri.parse("$image"),
+          title: title),
+    );
+    ShortDynamicLink shortDynamicLink = await dynamicLinks.buildShortLink(parameters);
+
+
+    String? desc = shortDynamicLink.shortUrl.toString();
+    print(desc);
+
+    // await Share.share(desc, subject: title,);
+
+  }
 }
